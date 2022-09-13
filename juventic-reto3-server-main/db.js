266b26cd0aeb2bd6a8057d2a761d2d0cab6b828e
khuaza9612 <andresnavia-96@hotmail.com>
2022-09-13 16:46:56 -0500

    carga de zise

--- juventic-reto3-server-main/db.js
+++ juventic-reto3-server-main/db.js
@@ -1,9 +1,15 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config(); 
+const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
 
- const sequelize = new Sequelize('postgres://postgres:Hk1143985102@localhost:5433/juventicc', {
+//  const sequelize = new Sequelize('postgres://postgres:Hk1143985102@localhost:5433/juventicc', {
+//   logging: false,
+
+
+// });
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
   logging: false,
 });
-
 
 module.exports = sequelize;
