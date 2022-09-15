28e83845819a3f8fc7a580032e4ffe1da1a39f57
khuaza9612 <andresnavia-96@hotmail.com>
2022-09-15 13:06:00 -0500

    carga de zise

--- juventic-reto3-main/src/server.js
+++ juventic-reto3-main/src/server.js
@@ -1,3 +1,3 @@
-const URL = process.env.NODE_ENV !== 'production' ? 'http://localhost:4000/api' : 'https://juventic-reto3-server.herokuapp.com/api';
+const URL = process.env.NODE_ENV !== 'production' ? 'https://restaurante-col.herokuapp.com/api' : '';
 
 export default URL;
